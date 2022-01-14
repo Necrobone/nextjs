ae8d2db33ec66798d88cc8d9295e389f55f9c823
Necrobone <necrobone@hotmail.com>
2022-01-14 20:53:20 +0100

    Fallback rendering

--- pages/meetups/[id]/index.js
+++ pages/meetups/[id]/index.js
@@ -9,4 +9,41 @@ function Meetup() {
     />;
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    id: 'm1',
+                },
+            },
+            {
+                params: {
+                    id: 'm2',
+                },
+            },
+        ],
+    };
+}
+
+export async function getStaticProps(context) {
+    // fetch
+    const meetupId = context.params.id;
+
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+                title: 'A First Meetup',
+                address: 'Some address5, 12345 Some City',
+                description: 'This is a first meetup!',
+            },
+        },
+    };
+}
+
 export default Meetup;
